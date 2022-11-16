3c1b390c75db46b6293124b7249f33ad176736ff
mjsimo99 <mohashark21@gmail.com>
2022-11-16 17:35:08 +0100

    fix links

--- club.php
+++ club.php
@@ -88,7 +88,7 @@ margin-bottom: 40px;">
 
 <!-- club end -->
 <!-- link for pages -->
-<a href="club.php" class="link-primary">club</a>
+<a href="login.php" class="link-primary">login</a>
 <a href="index.php" class="link-secondary">index</a>
 <a href="club.php" class="link-success">club</a>
 <a href="member.php" class="link-danger">member</a>
